d7dc3686108e46d619b91eda72380bc70561c013
Pcode999 <lottow5307@gmail.com>
2025-09-20 01:38:00 +0700

    add: something

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // allow external access
+    port: 5173,      // keep your Vite dev port
+    allowedHosts: [
+      'ec2-3-104-104-235.ap-southeast-2.compute.amazonaws.com'
+    ]
+  }
 })
